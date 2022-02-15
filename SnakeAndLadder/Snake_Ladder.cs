5e2b05fbe4339188734ca4cad6fb79775602e3da
Amitmagadum1545 <amitmagadum1008@gmail.com>
2022-02-15 20:00:45 +0530

    [Amit] Added Program To Check Play,Noplay Or Snake

--- SnakeAndLadder/Snake_Ladder.cs
+++ SnakeAndLadder/Snake_Ladder.cs
@@ -11,10 +11,25 @@ namespace SnakeAndLadder
         public static void Die_Roll()
         {
             int position = 0;
+            const int NOPLAY = 0, LADDER = 1, SNAKE = 2;
             Random random = new Random();
-            int check=random.Next(1,7);
+            int noOnDie=random.Next(1,7);
+            int option=random.Next(3);
             Console.WriteLine($"Initial Position Of Player is {position}");
-            Console.WriteLine($"Random Number Is {check}");
+            Console.WriteLine($"Number On Die Is {noOnDie}");
+            switch (option)
+            {
+                case NOPLAY :
+                    Console.WriteLine("POSITION " + position);
+                    break;
+                case LADDER:
+                    position = position + noOnDie;
+                    break;
+                case SNAKE:
+                    position = position - noOnDie;
+                    break;
+            }
+            Console.WriteLine($"Position Of Player After Die Roll is {position}");
         }
     }
 }
